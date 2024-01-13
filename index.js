3db1481cb83cdaba34997c13a79c34f2cee99ffe
v1ab1 <sharkandmouse@gmail.com>
2024-01-14 01:39:48 +0300

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+
+const app = express();
+const PORT = 3006;
+
+app.use('/uploads', serveStatic(path.join(__dirname, '..', 'uploads'))); // Specifies the folder with files
+
+app.get('/', (req, res) => {
+  res.status(200).send("Welcome to root URL of Server");
+});
+
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log("Server is Successfully Running, and App is listening on port " + PORT)
+  else
+    console.log("Error occurred, server can't start", error);
+});
