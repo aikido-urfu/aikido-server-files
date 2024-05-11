120a42fcc2bc112672f3efa62ef117c2e60b99f3
MintBrain <maximka602@gmail.com>
2024-05-12 01:15:52 +0500

    files path fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const PORT = 3006;
 
-app.use('/uploads', serveStatic(path.join(__dirname, '..', 'uploads'))); // Specifies the folder with files
+app.use('/uploads', serveStatic(path.join(__dirname, '..', 'aikido-server-core',  'uploads'))); // Specifies the folder with files
 
 app.get('/', (req, res) => {
   res.status(200).send("Welcome to root URL of Server");
